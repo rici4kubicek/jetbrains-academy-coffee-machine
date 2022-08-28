e18e39fef2b31c8e9699c71774c862d72e0ad3d9
Richard Kubíček <rici.kubicek@gmail.com>
2022-08-28 18:35:35 +0200

    Initial commit

---
+++ Coffee Machine/task/main.js
@@ -0,0 +1,6 @@
+// Use "input()" to input a line from the user
+// Use "input(str)" to print some text before requesting input
+// You will need this in the following stages
+const input = require('sync-input')
+
+console.log("Hello, World!")
