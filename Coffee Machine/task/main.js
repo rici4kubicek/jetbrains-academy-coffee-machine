b74e61c9bbe6db353736978d7e905ee534d04f2c
Richard Kubíček <rici.kubicek@gmail.com>
2022-08-28 18:37:13 +0200

    Stage 1

--- Coffee Machine/task/main.js
+++ Coffee Machine/task/main.js
@@ -3,4 +3,10 @@
 // You will need this in the following stages
 const input = require('sync-input')
 
-console.log("Hello, World!")
+console.log('Starting to make a coffee\n' +
+    'Grinding coffee beans\n' +
+    'Boiling water\n' +
+    'Mixing boiled water with crushed coffee beans\n' +
+    'Pouring coffee into the cup\n' +
+    'Pouring some milk into the cup\n' +
+    'Coffee is ready!')
