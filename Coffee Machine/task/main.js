5b1451f49a38c881a6bb5095d1a5c992012b2fad
Richard Kubíček <rici.kubicek@gmail.com>
2022-09-01 21:00:13 +0200

    Stage 3
    * unfortunately not my solution :(

--- Coffee Machine/task/main.js
+++ Coffee Machine/task/main.js
@@ -1,37 +1,33 @@
 // Use "input()" to input a line from the user
 // Use "input(str)" to print some text before requesting input
 // You will need this in the following stages
-const input = require('sync-input')
+const input = require('sync-input');
 
-let water = Number(input('Write how many ml of water the coffee machine has:\n'));
-let milk = Number(input('Write how many ml of milk the coffee machine has:\n'));
-let coffee = Number(input('Write how many grams of coffee beans the coffee machine has:\n'));
-let cups = Number(input('Write how many cups of coffee you will need:\n'));
-
-let availableCups = 0;
-
-if (cups * 200 >= water) {
-    availableCups = (water / 200).toFixed();
-    if (cups * 50 >= milk) {
-
-        if (cups * 15 >= coffee) {
-
-        }
-    }
+let coffee = {
+    water: 200, milk: 50, beans: 15
 }
 
 
-//console.log('Write how many cups of coffee you will need:\n');
+let hasWater = input(`Write how many ml of water the coffee machine has:`);
+let hasMilk = input(`Write how many ml of milk the coffee machine has:`);
+let hasBean = input(`Write how many grams of coffee beans the coffee machine has:`);
 
-console.log('For ' + cups + ' cups of coffee you will need:');
-console.log('' + 200 * cups + ' ml of water');
-console.log('' + 50 * cups + ' ml of milk');
-console.log('' + 15 * cups + ' g of coffee beans');
+let needCoffee = input(`Write how many cups of coffee you will need:`);
 
-/*console.log('Starting to make a coffee\n' +
-    'Grinding coffee beans\n' +
-    'Boiling water\n' +
-    'Mixing boiled water with crushed coffee beans\n' +
-    'Pouring coffee into the cup\n' +
-    'Pouring some milk into the cup\n' +
-    'Coffee is ready!')*/
+function availableCoffee() {
+    let waterForCoffee = hasWater / 200;
+    let milkForCoffee = hasMilk / 50;
+    let beanForCoffee = hasBean / 15;
+
+    return Math.floor(Math.min(waterForCoffee, milkForCoffee, beanForCoffee));
+}
+
+if (needCoffee > availableCoffee()) {
+    console.log(`No, I can make only ${availableCoffee()} cups of coffee`);
+} else if (needCoffee == availableCoffee()) {
+    console.log(`Yes, I can make that amount of coffee`);
+} else if (needCoffee < availableCoffee()) {
+    console.log(`Yes, I can make that amount  of coffee (and even ${availableCoffee() - needCoffee} more than that)`)
+}
+
+
